e76e1b368295e6937bac81912c72eb29d82cc7f9
Robert Waite <winstonwaite@gmail.com>
2016-09-24 22:41:19 -0700

    test file

---
+++ test-match.py
@@ -0,0 +1,70 @@
+from AlphaGo.models.policy import CNNPolicy
+from AlphaGo.ai import GreedyPolicyPlayer
+from AlphaGo.ai import ProbabilisticPolicyPlayer
+from AlphaGo.go import GameState
+from AlphaGo.util import save_gamestate_to_sgf
+import AlphaGo.go as go
+import uuid
+
+
+def playout(player1, player2, bd_size=19, print_game=False, player1_name='Player 1', player2_name='Player 2', save_dir='/alphago/playouts', save_name='record.sgf'):
+	gamestate = GameState(size=bd_size, enforce_superko=True)
+	counter = 0
+	# Play 10 games
+	while True:
+		counter += 1
+		move = player1.get_move(gamestate)
+		gamestate.do_move(move)
+		if gamestate.is_end_of_game:
+			break
+		move = player2.get_move(gamestate)
+		gamestate.do_move(move)
+		if gamestate.is_end_of_game:
+			break
+		if counter > 1000:
+			print 'found excessively long game'
+			break
+
+	print counter
+	if print_game:
+		save_gamestate_to_sgf(gamestate, save_dir, save_name, player1_name, player2_name)
+
+	return gamestate.get_winner()
+
+
+model = '/alphago/SLv1/my_model.json'
+player1_weights = '/alphago/competition/weights.RLTEST.hdf5'
+player2_weights = '/alphago/competition/weights.00006-gold-third-.002lr.hdf5'
+player1_policy = CNNPolicy.load_model(model)
+player1_policy.model.load_weights(player1_weights)
+player2_policy = CNNPolicy.load_model(model)
+player2_policy.model.load_weights(player2_weights)
+
+player1 = GreedyPolicyPlayer(player1_policy)
+player2 = GreedyPolicyPlayer(player2_policy)
+
+print playout(player1, player2, print_game=True, save_name="as_black.sgf")
+print playout(player2, player1, print_game=True, save_name="as_white.sgf")
+
+playouts = 100
+
+player1 = ProbabilisticPolicyPlayer(player1_policy, temperature=.67)
+player2 = ProbabilisticPolicyPlayer(player2_policy, temperature=.67)
+player1_wins = 0.0
+for i in range(0, playouts):
+	filename = str(uuid.uuid4()) + '.sgf'
+	winner = playout(player1, player2, player1_name='player1', player2_name='player2', print_game=False, save_name=filename)
+	print winner
+	if winner == go.BLACK:
+		player1_wins += 1
+	filename = str(uuid.uuid4()) + '.sgf'
+	winner = playout(player2, player1, player1_name='player2', player2_name='player1', print_game=False, save_name=filename)
+	print winner
+	if winner == go.WHITE:
+		player1_wins += 1
+
+print player1_wins
+print player1_wins / (playouts * 2.0)
+
+
+
